c226bcaeb49cc9a36e04f7b3949384b6510f7958
eb46 <ericjbondoc@gmail.com>
2020-07-14 11:03:07 -0700

    all CRUD routes moved to desk_controller.js file. bcrypt package installed on back-end

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,11 @@
 // Dependencies
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const desk_pool = require('./database.js')
+const session = require('express-session')
+
+// Configuration
+const app = express()
 const PORT = 5000
 
 // Middleware
@@ -12,69 +15,17 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
 // Controllers
+const deskController = require('./controllers/desk_controller.js')
+app.use('/', deskController)
 
-// const spacesController = require('./controllers/spaces_controller.js')
+// const usersController = require('./controllers/users_controller.js')
+// app.use('/users', usersController)
+//
+// const sessionsController = require('./controllers/sessions_controller.js')
+// app.use('/sessions', sessionsController)
 
 // Routes
 
-// CREATE
-app.post('/desks', async (req, res) => {
-  try {
-    const { username, image } = req.body
-    const newDesk = await desk_pool.query("INSERT INTO desks (username, image) VALUES($1, $2) RETURNING *",
-    [username, image])
-    res.json(newDesk.rows)
-  } catch (err) {
-    console.log(err)
-  }
-})
-
-// READ
-app.get('/desks', async (req, res) => {
-  try {
-    const allDesks = await desk_pool.query("SELECT * FROM desks")
-    res.json(allDesks.rows)
-  } catch (err) {
-    console.log(err)
-  }
-})
-
-app.get('/desks/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const desk = await desk_pool.query("SELECT * FROM desks WHERE id = $1", [id])
-    res.json(desk.rows[0])
-  } catch (err) {
-    console.log(err)
-  }
-})
-
-// UPDATE
-app.put('/desks/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const { username, image } = req.body
-    const updateDesk = await desk_pool.query(
-      "UPDATE desks SET username = $1, image = $2 WHERE id = $3 RETURNING *",
-      [username, image, id]
-    )
-    console.log(desk.rows)
-    res.json(updateDesk.rows)
-  } catch (err) {
-    console.log(err)
-  }
-})
-
-// DELETE
-app.delete('/desks/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const deleteDesk = await desk_pool.query("DELETE FROM desks WHERE id = $1 RETURNING *", [id])
-
-  } catch (err) {
-    console.log(err)
-  }
-})
 
 
 app.listen(PORT, () => {
